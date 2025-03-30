9c2371f75fe57cb1ff41e6664995cd52fb756bbc
kinge <kingershy@gmail.com>
2025-03-30 16:02:19 +0300

    generate 400 employees

---
+++ data insert/generatedata.sql
@@ -0,0 +1,480 @@
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (7894,"Stephen Malone"),
+  (6485,"Griffin Chen"),
+  (318,"Demetrius Schwartz"),
+  (794,"Jelani Bell"),
+  (2453,"Steven Powers"),
+  (7070,"Hedy Rodriguez"),
+  (9839,"Justin Hobbs"),
+  (6719,"Brenden Estrada"),
+  (1820,"Lani Walter"),
+  (2742,"Belle Lindsay");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (5069,"Melodie Golden"),
+  (5058,"Garrett Le"),
+  (9832,"Blossom Robbins"),
+  (6721,"Keely Hubbard"),
+  (6454,"Caldwell Rush"),
+  (4294,"Demetria Riley"),
+  (1410,"Jason Buck"),
+  (2203,"Gray Charles"),
+  (231,"Vivian Mayer"),
+  (6480,"Xyla Mayer");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (6283,"Ivan Mcbride"),
+  (7207,"Jack Mcbride"),
+  (556,"Alika Vasquez"),
+  (6622,"Armando Christensen"),
+  (975,"Desiree Moran"),
+  (1695,"Rae Leblanc"),
+  (989,"Kristen Beach"),
+  (3859,"Elliott Rogers"),
+  (6205,"Troy Gonzalez"),
+  (8128,"Charlotte Stanton");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (116,"Keegan Reyes"),
+  (5935,"Desirae Griffin"),
+  (6967,"Ferdinand Livingston"),
+  (9904,"Harriet Levy"),
+  (4186,"Serina Goff"),
+  (4405,"Hilel Stanton"),
+  (9081,"Quemby Vang"),
+  (3657,"Lani Howard"),
+  (5035,"Blythe Valdez"),
+  (3214,"Amy Snider");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (969,"Chanda Estrada"),
+  (6790,"Jamalia Mathis"),
+  (7768,"Cody Dunlap"),
+  (527,"Addison Pitts"),
+  (5169,"Ivan Lowe"),
+  (2499,"Emily Mckenzie"),
+  (848,"Jenette Kemp"),
+  (1134,"Lee Wilder"),
+  (293,"Josiah Heath"),
+  (2621,"Remedios Chan");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (6276,"Igor Armstrong"),
+  (326,"Brendan Stout"),
+  (8397,"Octavius Gray"),
+  (3340,"Echo Kaufman"),
+  (8510,"Cathleen Williams"),
+  (9664,"Desiree Pena"),
+  (8678,"Rinah Richardson"),
+  (7062,"Dante James"),
+  (9679,"Nasim Ware"),
+  (9378,"Urielle Small");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (2666,"Ebony Stark"),
+  (822,"Ariel Beach"),
+  (4029,"Shaine Dickerson"),
+  (7229,"Maile Browning"),
+  (9272,"Pandora Conrad"),
+  (198,"Cameron Navarro"),
+  (8992,"Chelsea Hines"),
+  (593,"Piper Joyce"),
+  (9463,"Jessica Rodriguez"),
+  (7809,"Blake Welch");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (9502,"Flynn Franco"),
+  (2004,"Adam Matthews"),
+  (6522,"Bo Kane"),
+  (2708,"Regan Bridges"),
+  (5027,"Raya Navarro"),
+  (2620,"Ocean Stout"),
+  (3559,"Zoe Greene"),
+  (3682,"Brooke Pollard"),
+  (7176,"Clarke Ewing"),
+  (8158,"Noel Morales");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (8970,"Reagan Thornton"),
+  (9893,"Kyla Rasmussen"),
+  (586,"Jelani Wolfe"),
+  (7139,"Vaughan Johnson"),
+  (7181,"Zelenia Burke"),
+  (948,"Odessa Lowery"),
+  (3290,"Katell Little"),
+  (1651,"Kelly Brown"),
+  (2631,"Mary Le"),
+  (9832,"Jarrod Mcconnell");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (3845,"Evan Smith"),
+  (2078,"Odessa Morris"),
+  (857,"Norman Crosby"),
+  (7497,"Roary Bradford"),
+  (7832,"Palmer Watts"),
+  (422,"Gavin Case"),
+  (4060,"Heidi Vaughn"),
+  (2870,"Demetria Hoffman"),
+  (3558,"Joel Weeks"),
+  (7374,"Damon Edwards");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (4529,"Perry Church"),
+  (5571,"Macaulay Solis"),
+  (9858,"Zena Gallegos"),
+  (9337,"Ezekiel Mayo"),
+  (4244,"Tatiana Woodward"),
+  (3085,"Henry Church"),
+  (8642,"Odysseus Calhoun"),
+  (5024,"Hannah Reilly"),
+  (4861,"Kenyon Puckett"),
+  (325,"Gray Castillo");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (4845,"Steven Bullock"),
+  (1060,"Garrett Collins"),
+  (7578,"Sigourney Knowles"),
+  (8502,"Breanna Sweeney"),
+  (504,"Halee Bailey"),
+  (2731,"May Snider"),
+  (4122,"Eugenia Garner"),
+  (6928,"Kai Pace"),
+  (9931,"Lynn Puckett"),
+  (3504,"Dahlia Sullivan");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (90,"Ray Wong"),
+  (748,"Fletcher Kaufman"),
+  (5346,"Shaeleigh Fuentes"),
+  (4262,"Quinn Wise"),
+  (4010,"Kennedy Valentine"),
+  (7187,"Lucian Noel"),
+  (9968,"Courtney Tran"),
+  (3475,"Clayton Hurst"),
+  (8064,"Oprah Gould"),
+  (702,"Hiroko Whitley");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (6207,"Sharon Burch"),
+  (5643,"Sonya Moran"),
+  (758,"Wesley Albert"),
+  (9603,"Tashya Savage"),
+  (7078,"Davis Leon"),
+  (396,"Dominique Sykes"),
+  (3739,"Wylie Marshall"),
+  (9182,"Latifah Ortiz"),
+  (5940,"Burke Wilcox"),
+  (7785,"Kelly Mercado");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (4570,"Caldwell Porter"),
+  (8375,"Candace Grant"),
+  (7112,"Jarrod Mcneil"),
+  (8227,"Kyla Summers"),
+  (5210,"Xaviera Chapman"),
+  (8142,"Nigel Emerson"),
+  (6894,"Wendy Harding"),
+  (6404,"Vivien Horn"),
+  (8153,"Sasha Coffey"),
+  (8058,"Martina Norton");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (2406,"Magee Hester"),
+  (6350,"Isabella Gray"),
+  (7768,"Sheila Robinson"),
+  (1332,"Guy Pugh"),
+  (8360,"Mannix Robertson"),
+  (9296,"Ainsley Battle"),
+  (3988,"Aristotle Faulkner"),
+  (5742,"Bertha Andrews"),
+  (5706,"Abra Weaver"),
+  (7435,"Cody Frost");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (9537,"Derek Dawson"),
+  (8021,"Imogene Dixon"),
+  (543,"Oliver Craig"),
+  (4811,"Virginia Owen"),
+  (7466,"Megan Knowles"),
+  (1571,"Brynn Moss"),
+  (9114,"Kelsie Velez"),
+  (5802,"Audrey Gay"),
+  (996,"Brynn Matthews"),
+  (876,"Alfonso Carver");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (7209,"Whilemina Castillo"),
+  (4974,"John Ratliff"),
+  (6848,"Prescott Sexton"),
+  (2053,"Lydia Gill"),
+  (8938,"Stella Powell"),
+  (3992,"Keelie Ray"),
+  (8938,"Jelani Logan"),
+  (1046,"Damian Abbott"),
+  (5926,"Regina Harrell"),
+  (473,"Jessamine Black");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (5675,"Clark Perez"),
+  (825,"Hope Martin"),
+  (8791,"Idona Keith"),
+  (5171,"Dane Talley"),
+  (6405,"Shea England"),
+  (6865,"Arsenio Franklin"),
+  (4466,"Freya Houston"),
+  (7608,"Rebekah Mosley"),
+  (2066,"Alan Mckinney"),
+  (3415,"Anjolie Moreno");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (6809,"Amelia Bird"),
+  (3654,"Nina Gay"),
+  (9223,"Cruz Brooks"),
+  (3940,"Prescott Roberts"),
+  (7437,"Noelani Compton"),
+  (1247,"Charde Carson"),
+  (4373,"Linda Sampson"),
+  (9941,"Ralph Ortiz"),
+  (6049,"Idona Osborn"),
+  (869,"Gil Head");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (8034,"Mark Ware"),
+  (4091,"Naida Glass"),
+  (297,"Philip Mcpherson"),
+  (6302,"Camden Caldwell"),
+  (8848,"Lenore Cooper"),
+  (6360,"Mona Tran"),
+  (1927,"Cullen Dickerson"),
+  (5151,"Yetta Woodard"),
+  (2006,"Dorian Barrett"),
+  (6666,"Oliver Head");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (2012,"Melissa Garrison"),
+  (9918,"Dillon Gross"),
+  (4613,"Hayley Osborn"),
+  (8553,"Orla Abbott"),
+  (5045,"Deborah Finley"),
+  (2652,"Addison Rutledge"),
+  (3402,"Tarik Barrera"),
+  (4114,"Dahlia Merritt"),
+  (7529,"Fletcher Hudson"),
+  (1825,"Winter Pate");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (7531,"Pearl Hale"),
+  (1044,"Jonah Sosa"),
+  (9783,"Moses Park"),
+  (4460,"Abraham Moran"),
+  (34,"Daria Roy"),
+  (2246,"Logan Benjamin"),
+  (3869,"John Valenzuela"),
+  (3626,"Jolie Mueller"),
+  (1202,"Chloe Sampson"),
+  (8537,"Macaulay Myers");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (3832,"Farrah Humphrey"),
+  (8077,"Daquan Mccoy"),
+  (2719,"Byron Stein"),
+  (3526,"Jescie Burris"),
+  (3340,"Naida Boyle"),
+  (9884,"Lane Hebert"),
+  (3728,"Kelly Dawson"),
+  (3464,"Gisela Mclaughlin"),
+  (9094,"Kennan Hines"),
+  (3918,"Channing Stokes");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (639,"Nola Nixon"),
+  (1675,"Lillith Beck"),
+  (5199,"Lester Hendrix"),
+  (3758,"Yetta Arnold"),
+  (4666,"Selma Mccormick"),
+  (3399,"Idona Rodgers"),
+  (1095,"Megan Ewing"),
+  (7157,"Beck Mcdowell"),
+  (1024,"Reagan Hayden"),
+  (9020,"Hanna Norman");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (9477,"Kameko Norris"),
+  (6410,"Jamalia Kelley"),
+  (2685,"Erica Case"),
+  (4579,"Alice Richardson"),
+  (6269,"Warren Wise"),
+  (3289,"Ariel Hampton"),
+  (7809,"Keaton Tran"),
+  (3499,"Adrienne Mayo"),
+  (3294,"Celeste Carson"),
+  (9853,"Dean Ayers");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (625,"Doris Pope"),
+  (6810,"Cameron Odom"),
+  (8361,"Ivana Gray"),
+  (8130,"Amethyst Rutledge"),
+  (8686,"Russell Coleman"),
+  (8137,"Sean Higgins"),
+  (5376,"Diana Guzman"),
+  (6669,"Dara Mcdowell"),
+  (9714,"Alexandra Wiley"),
+  (5359,"Yoshi Gallagher");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (9593,"Paul Massey"),
+  (2034,"Fallon Caldwell"),
+  (6171,"Nasim Holt"),
+  (6755,"Marny Lawson"),
+  (8877,"Dahlia Patel"),
+  (9301,"Murphy Sampson"),
+  (6610,"Tashya Mccarty"),
+  (9582,"Allen Workman"),
+  (4507,"Len Suarez"),
+  (812,"Clare Bradshaw");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (5362,"Cheryl Schultz"),
+  (2169,"Rae Fitzgerald"),
+  (7328,"Scarlet Waters"),
+  (3125,"Aurelia Mann"),
+  (4345,"Calista Ayala"),
+  (8557,"Imelda Giles"),
+  (8830,"Brittany Barker"),
+  (5704,"Leilani Herrera"),
+  (3497,"Francis Vasquez"),
+  (1793,"Vernon Mccray");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (226,"Ava Nieves"),
+  (4834,"Beverly Byrd"),
+  (4101,"Shana Hooper"),
+  (4648,"Abdul Mccoy"),
+  (8400,"Nayda Hunt"),
+  (6150,"Upton Salinas"),
+  (7794,"Barclay Fisher"),
+  (9599,"Orlando Morrow"),
+  (4699,"Harrison Pruitt"),
+  (1366,"Destiny Wilson");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (9570,"Quynn Thomas"),
+  (6186,"Reese Bradley"),
+  (1641,"Baxter Mendez"),
+  (836,"Kylan Padilla"),
+  (3116,"Amena Douglas"),
+  (7471,"Marvin Keller"),
+  (7001,"Cameron Briggs"),
+  (2003,"Ryan Chaney"),
+  (7976,"Davis Hubbard"),
+  (3154,"Xenos Blackburn");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (7951,"Scott Simon"),
+  (3855,"Robert Schroeder"),
+  (6693,"Chava Ashley"),
+  (7067,"Nehru Bentley"),
+  (706,"Denton Barr"),
+  (4476,"Ivan Oneil"),
+  (5328,"Murphy Turner"),
+  (3556,"Cody Rhodes"),
+  (5258,"Carla Sherman"),
+  (984,"Jordan Gibbs");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (9103,"Ulla Barry"),
+  (4910,"Elton Eaton"),
+  (1332,"Candace Hess"),
+  (2967,"Catherine Sosa"),
+  (6465,"Barrett Sears"),
+  (3589,"Kuame Medina"),
+  (1346,"Veda Pena"),
+  (7039,"Yardley Justice"),
+  (5129,"Kelly Fleming"),
+  (7219,"Arsenio Roberson");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (9587,"Magee Castro"),
+  (5125,"Shoshana Ratliff"),
+  (1680,"Kirsten Chaney"),
+  (5520,"Chloe Parks"),
+  (7732,"Jane Drake"),
+  (9274,"Sade Dixon"),
+  (3697,"Randall Zimmerman"),
+  (1483,"Magee Kirk"),
+  (8798,"Ebony Kirby"),
+  (5033,"Rylee Forbes");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (7103,"Colin Carver"),
+  (6092,"Ashton Coleman"),
+  (4032,"Reagan Reeves"),
+  (1049,"Camilla Hunter"),
+  (7913,"Brandon Byrd"),
+  (1643,"Valentine Raymond"),
+  (4540,"Angela Miles"),
+  (61,"Jeanette Lewis"),
+  (3068,"Raja Daniel"),
+  (9121,"Ross Browning");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (7786,"Micah Vasquez"),
+  (3616,"Patricia Hull"),
+  (2168,"Daphne Navarro"),
+  (4372,"Miranda Petersen"),
+  (7918,"Lillian Leblanc"),
+  (7404,"Carla West"),
+  (6034,"Amelia Mcclure"),
+  (8761,"Aquila Hale"),
+  (5769,"Athena Shaw"),
+  (5630,"Evangeline Long");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (7924,"Darrel Mclean"),
+  (7821,"Tad Mclean"),
+  (7457,"Lucian Brewer"),
+  (5476,"India Fernandez"),
+  (2093,"Adrienne Forbes"),
+  (8730,"Oliver Stephenson"),
+  (4569,"Kim Levine"),
+  (2990,"Randall Stone"),
+  (7428,"Emma Hardy"),
+  (5440,"Kirk Byers");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (831,"Tucker Higgins"),
+  (8074,"Farrah Hill"),
+  (6765,"Imani Rodriguez"),
+  (8814,"Benedict Wilcox"),
+  (2244,"Calvin Kidd"),
+  (515,"August Mueller"),
+  (4603,"Stacy Mcintyre"),
+  (9263,"Erich Marshall"),
+  (5142,"Buckminster Stuart"),
+  (8806,"Petra Howe");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (1186,"Elvis Matthews"),
+  (2494,"Todd Glass"),
+  (9207,"Zorita Zamora"),
+  (670,"Dane Vang"),
+  (3414,"Anthony Rutledge"),
+  (9255,"Alyssa Chavez"),
+  (8970,"Walker Travis"),
+  (2238,"Justin Bishop"),
+  (6425,"Jillian Petersen"),
+  (9893,"Reuben Small");
+INSERT INTO `Employee` (`EmployeeID`,`Name`)
+VALUES
+  (2564,"Cole Anderson"),
+  (646,"Jillian Hampton"),
+  (6638,"Claire Terrell"),
+  (86,"Jael Cobb"),
+  (7353,"Deirdre Frank"),
+  (4749,"Deanna Mcfarland"),
+  (1132,"Burke Bean"),
+  (3914,"Axel Hunter"),
+  (3332,"Quail Kirby"), 
+  (1824,"Aristotle Joyce");
