7782355163c9f23db5ed1b6cc185898bf52a48bb
orelm123 <orelmaslati@gmail.com>
2025-05-07 15:48:11 +0300

    Update Constraints.sql

--- part2/Constraints.sql
+++ part2/Constraints.sql
@@ -1,12 +1,12 @@
 
--- אילוץ CHECK לתאריך לידה בעתיד
+-- Customer: Prevents inserting or updating a customer with a future birth date.
 ALTER TABLE Customer
 ADD CONSTRAINT chk_birth_date CHECK (date_of_birth < CURRENT_DATE);
 
--- אילוץ DEFAULT לסוג כתובת
+-- Address.address_type: Sets the default address type to 'Home' when none is provided.
 ALTER TABLE Address
 ALTER COLUMN asress_type SET DEFAULT 'Home';
 
--- אילוץ NOT NULL לכתובת אימייל
+-- Contact.contact_value: Ensures every contact must have a non-null email or phone value.
 ALTER TABLE Contact
 ALTER COLUMN contact_value SET NOT NULL;
